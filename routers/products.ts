d7f6b237cc3903802e9436d97aa218b17a1100d4
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2024-08-27 20:23:16 +0600

    Lesson 81 MongoDB

--- routers/products.ts
+++ routers/products.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Product, ProductMutation } from '../types';
+import { ProductMutation } from '../types';
 import {imagesUpload} from '../multer';
 import mongoDb from '../mongoDb';
 import { ObjectId } from 'mongodb';
