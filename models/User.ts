216c1abfb93ebab2a169fc4d864e48b1f2cbafdd
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2024-09-11 12:12:04 +0600

    Lesson 85

--- models/User.ts
+++ models/User.ts
@@ -12,6 +12,13 @@ const UserSchema = new Schema<UserFields, UserModel, UserMethods>({
     type: String,
     required: true,
     unique: true,
+    validate: {
+      validator: async function(value: string): Promise<boolean> {
+        const user = await User.findOne({username: value});
+        return !user;
+      },
+      message: 'This user is already registered!',
+    }
   },
   password: {
     type: String,
